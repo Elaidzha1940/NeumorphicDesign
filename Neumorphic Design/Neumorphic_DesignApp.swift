d9c2f029ebc9cd5fe0c6cc64ab9992630d31e5df
Elaidzha1940 <eli080293@gmail.com>
2023-06-02 21:01:57 +0700

    Alignment Tab Bar Button Animation View

---
+++ Neumorphic Design/Neumorphic_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neumorphic_DesignApp.swift
+//  Neumorphic Design
+//
+//  Created by Elaidzha Shchukin on 02.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Neumorphic_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
